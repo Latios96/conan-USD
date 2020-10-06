19411dca10a13832ae14379e16c6c9cfff77f4fa
Jan <jan.honsbrok@gmail.com>
2020-10-06 11:06:30 +0200

    dont do brew mess

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,17 +4,7 @@ set -e
 set -x
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
-    brew install pyenv-virtualenv
-    brew install cmake || true
 
-    if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
-    fi
-
-    pyenv install 2.7.10
-    pyenv virtualenv 2.7.10 conan
-    pyenv rehash
-    pyenv activate conan
 fi
 
 pip install conan --upgrade
