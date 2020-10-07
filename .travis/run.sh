e5cdfc98fa884cc1211bee2e5f6d77b8c73dcabc
Jan Honsbrok <jan.honsbrok@gmail.com>
2020-10-07 15:47:38 +0200

    use ConanPackageTools

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,4 +3,5 @@
 set -e
 set -x
 
-conan create . Latios96/stable
+#conan create . Latios96/stable
+python build.py
--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(archs=["x86_64"])
     builder.add_common_builds()
     builder.build_policy = "missing"
     builder.run()
