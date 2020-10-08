6361896fa8b39d76acea77076d85c15d14770098
Jan Honsbrok <jan.honsbrok@gmail.com>
2020-10-08 17:52:13 +0200

    fix libc++ for linux

--- build.py
+++ build.py
@@ -1,7 +1,12 @@
+import sys
+
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(archs=["x86_64"])
     builder.add_common_builds()
     builder.build_policy = "missing"
+    if sys.platform == "linux":
+        for settings, options, env_vars, build_requires, reference in builder.items:
+            settings["compiler.libcxx"] = "libstdc++11"
     builder.run()
