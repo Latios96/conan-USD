b2eed07c88d10466768950efa8aa351830acb5c8
Jan Honsbrok <jan.honsbrok@gmail.com>
2020-10-08 19:01:47 +0200

    np static on linux

--- build.py
+++ build.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(archs=["x86_64"])
     builder.add_common_builds()
     builder.build_policy = "missing"
-    if sys.platform == "linux":
-        for settings, options, env_vars, build_requires, reference in builder.items:
-            settings["compiler.libcxx"] = "libstdc++11"
+    builder.remove_build_if(
+        lambda build: build.settings["USD:shared"] == True and sys.platform == "linux")
     builder.run()
