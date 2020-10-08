e7ce96c9b33b4abbfb1aa1ae1ac57ac3d67f3e8d
Jan Honsbrok <jan.honsbrok@gmail.com>
2020-10-08 19:05:26 +0200

    no static libs

--- build.py
+++ build.py
@@ -2,10 +2,11 @@ import sys
 
 from cpt.packager import ConanMultiPackager
 
+
 if __name__ == "__main__":
     builder = ConanMultiPackager(archs=["x86_64"])
     builder.add_common_builds()
     builder.build_policy = "missing"
     builder.remove_build_if(
-        lambda build: build.settings["USD:shared"] == True and sys.platform == "linux")
+        lambda build: build.options["USD:shared"] == True and sys.platform == "linux")
     builder.run()
