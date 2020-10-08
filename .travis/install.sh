07ea7d721a888bf4f1eb35c7a709728d757b0541
Jan Honsbrok <jan.honsbrok@gmail.com>
2020-10-08 17:27:52 +0200

    only create conan profile if missing

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,7 +8,13 @@ pip3 install conan --upgrade
 pip3 install conan_package_tools
 
 conan user
-conan profile new default --detect
+conanDefaultProfile=~/.conan/profiles/default
+if [ -f "$conanDefaultProfile" ]; then
+  echo "Conan default profile already exists!"
+else
+  echo "Conan default profile not exists, creating.."
+  conan profile new default --detect
+fi
 
 if [[ "$(uname -s)" != 'Darwin' ]]; then
   conan profile update settings.compiler.libcxx=libstdc++11 default
