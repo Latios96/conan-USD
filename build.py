df8855c1167a62a20456a549d64e61d5963c140c
Jan <jan.honsbrok@gmail.com>
2020-11-03 08:36:26 +0100

    reformat Python code

--- build.py
+++ build.py
@@ -6,12 +6,18 @@ from cpt.packager import ConanMultiPackager
 def skip_if_windows(build):
     if sys.platform != "win32":
         return False
-    return build.settings['build_type'] and build.settings['compiler.runtime'].startswith("MT")
+    return build.settings["build_type"] and build.settings[
+        "compiler.runtime"
+    ].startswith("MT")
+
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(archs=["x86_64"])
     builder.add_common_builds()
     builder.build_policy = "missing"
     builder.remove_build_if(
-        lambda build: build.options["USD:shared"] == False and sys.platform == "linux" or skip_if_windows(build))
+        lambda build: build.options["USD:shared"] == False
+        and sys.platform == "linux"
+        or skip_if_windows(build)
+    )
     builder.run()
